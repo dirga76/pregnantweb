c0d52b988ea689c195c53fc6c9e0b80bbdbce1ab
dirga76 <87862021+dirga76@users.noreply.github.com>
2023-06-18 00:00:29 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    color: {
+      bagan: "#346A94",
+    },
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      HP: "320px",
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      iphone: "375px",
+      xr: "414px",
+      x1: "185px",
+    },
+  },
+};
